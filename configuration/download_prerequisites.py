13f58cf877309a276ef749291759645e3ded5b0c
Oleg <solovyov.cxx@gmail.com>
2017-12-05 23:21:31 +0300

    move mac os prerequisites to python script

---
+++ configuration/download_prerequisites.py
@@ -0,0 +1,8 @@
+import os
+import platform
+
+if platform.system() = 'Darwin':
+	os.system('ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"')
+	os.sytem('brew install cmake')
+	os.sytem('brew install python3')
+	os.sytem('brew install conan')
--- configuration/mac_install_ requirements.sh
+++
@@ -1,6 +0,0 @@
-#! /usr/bin/env bash
-ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-brew install cmake
-brew install python3
-brew install conan
-#etc.
