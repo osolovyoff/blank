af3089f5d6013e5ea8f64ff21342796fcba99082
Oleg <solovyov.cxx@gmail.com>
2017-12-05 23:24:20 +0300

    fix smth syntax error :(

--- configuration/download_prerequisites.py
+++ configuration/download_prerequisites.py
@@ -1,8 +1,10 @@
 import os
 import platform
 
-if platform.system() = 'Darwin':
+if platform.system() == 'Darwin':
 	os.system('ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"')
-	os.sytem('brew install cmake')
-	os.sytem('brew install python3')
-	os.sytem('brew install conan')
+	os.system('brew install cmake')
+	os.system('brew install python3')
+	os.system('brew install conan')
+
+os.system('conan remote add conan-community https://api.bintray.com/conan/conan-community/conan')
